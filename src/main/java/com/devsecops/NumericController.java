a8a1aeb9fb0ed8ee6e1f82275ebd3a34c3f0ce89
kali <kali@kali.(none)>
2022-04-06 08:31:45 -0400

    jj

--- src/main/java/com/devsecops/NumericController.java
+++ src/main/java/com/devsecops/NumericController.java
@@ -20,6 +20,7 @@ public class NumericController {
 	private final Logger logger = LoggerFactory.getLogger(getClass());
 	private static final String baseURL = "http://localhost:5000/plusone";
 	
+	
 	RestTemplate restTemplate = new RestTemplate();
 	
 	@RestController
