14e507d306ca5c02e077acb119257ada31baa8c8
Nathan Rogers <nrogers.careers@gmail.com>
2022-09-11 19:03:28 -0600

    updating to openjdk-11-jdk as required by jenkins

--- setup/vm-install-script/install-script.sh
+++ setup/vm-install-script/install-script.sh
@@ -61,7 +61,7 @@ kubectl get node -o wide
 
 
 echo ".........----------------#################._.-.-Java and MAVEN-.-._.#################----------------........."
-sudo apt install openjdk-8-jdk -y
+sudo apt install openjdk-11-jdk -y
 java -version
 sudo apt install -y maven
 mvn -v
