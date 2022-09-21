86dd465345a4f7c04545585975c97f9e738d211c
Alceu Rodrigues de Freitas Junior <glasswalk3r@yahoo.com.br>
2022-09-21 19:35:28 -0300

    fix: string interpolation
    Proper string interpolation to avoid error in case of special chars.
    Also small fixes regarding spaces, tabs and new line chars.

--- setup/jenkins-plugins/installer.sh
+++ setup/jenkins-plugins/installer.sh
@@ -2,11 +2,11 @@
 
 set -eo pipefail
 
-JENKINS_URL="http://localhost:8080"
+JENKINS_URL='http://localhost:8080'
 
-JENKINS_CRUMB=$(curl -s --cookie-jar /tmp/cookies -u admin:admin $JENKINS_URL/crumbIssuer/api/json  | jq .crumb -r) 
+JENKINS_CRUMB=$(curl -s --cookie-jar /tmp/cookies -u admin:admin ${JENKINS_URL}/crumbIssuer/api/json | jq .crumb -r)
 
-JENKINS_TOKEN=$(curl -s -X POST -H "Jenkins-Crumb:"${JENKINS_CRUMB}"" --cookie /tmp/cookies  $JENKINS_URL/me/descriptorByName/jenkins.security.ApiTokenProperty/generateNewToken?newTokenName=\demo-token66 -u admin:admin | jq .data.tokenValue -r) 
+JENKINS_TOKEN=$(curl -s -X POST -H "Jenkins-Crumb:${JENKINS_CRUMB}" --cookie /tmp/cookies "${JENKINS_URL}/me/descriptorByName/jenkins.security.ApiTokenProperty/generateNewToken?newTokenName=demo-token66" -u admin:admin | jq .data.tokenValue -r)
 
 echo $JENKINS_URL
 echo $JENKINS_CRUMB
@@ -14,7 +14,7 @@ echo $JENKINS_TOKEN
 
 while read plugin; do
    echo "........Installing ${plugin} .."
-   curl -s POST --data "<jenkins><install plugin='${plugin}' /></jenkins>" -H 'Content-Type: text/xml' $JENKINS_URL/pluginManager/installNecessaryPlugins --user admin:$JENKINS_TOKEN
+   curl -s POST --data "<jenkins><install plugin='${plugin}' /></jenkins>" -H 'Content-Type: text/xml' "$JENKINS_URL/pluginManager/installNecessaryPlugins" --user "admin:$JENKINS_TOKEN"
 done < plugins.txt
 
 
