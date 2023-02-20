d94390cea12eae3d780a46804ff43a97e58da375
Antonio Bonanno <a.bonanno@gmail.com>
2023-02-21 00:07:26 +0100

    fixed reference to weave cni plugin

--- setup/vm-install-script/install-script.sh
+++ setup/vm-install-script/install-script.sh
@@ -50,7 +50,7 @@ kubeadm init --kubernetes-version=${KUBE_VERSION} --skip-token-print
 mkdir -p ~/.kube
 sudo cp -i /etc/kubernetes/admin.conf ~/.kube/config
 
-kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
+kubectl apply -f https://github.com/weaveworks/weave/releases/download/v2.8.1/weave-daemonset-k8s.yaml
 
 sleep 60
 
