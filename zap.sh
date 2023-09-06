98697cb54ff514b79393944ab47f85af39abaa7a
So Cool <127.0.0.1.pm@gmail.com>
2023-09-06 21:34:14 +0700

    add zap stage

---
+++ zap.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+PORT=$(kubectl -n default get svc ${serviceName} -o json | jq .spec.ports[].nodePort)
+
+# first run this
+chmod 777 $(pwd)
+echo $(id -u):$(id -g)
+docker run -v $(pwd):/zap/wrk/:rw -t owasp/zap2docker-weekly zap-api-scan.py -t $applicationURL:$PORT/v3/api-docs -f openapi -r zap_report.html
+
+exit_code=$?
+
+# comment above cmd and uncomment below lines to run with CUSTOM RULES
+# docker run -v $(pwd):/zap/wrk/:rw -t owasp/zap2docker-weekly zap-api-scan.py -t $applicationURL:$PORT/v3/api-docs -f openapi -c zap-rules -w report.md -J json_report.json -r zap_report.html
+
+# HTML Report
+sudo mkdir -p owasp-zap-report
+sudo mv zap_report.html owasp-zap-report
+
+echo "Exit Code : $exit_code"
+
+if [[ ${exit_code} -ne 0 ]]; then
+	echo "OWASP ZAP Report has either Low/Medium/High Risk. Please check the HTML Report"
+	exit 1
+else
+	echo "OWASP ZAP did not report any Risk"
+fi
