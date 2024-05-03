48afe1e4efcc0895887410562c6dfea9e40ec21f
Abdallah Musa <abdhalla.musa@fefundinfo.com>
2024-05-03 21:34:12 +0100

    quality gate fix again

--- src/main/java/com/devsecops/NumericController.java
+++ src/main/java/com/devsecops/NumericController.java
@@ -9,36 +9,33 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
-class NumericController {
+public class NumericController {
 
     private final Logger logger = LoggerFactory.getLogger(getClass());
     private static final String BASE_URL = "http://node-service:5000/plusone";
     private final RestTemplate restTemplate = new RestTemplate();
 
-    class Compare {
+    @GetMapping("/")
+    String welcome() {
+        return "Kubernetes DevSecOps";
+    }
 
-        @GetMapping("/")
-        String welcome() {
-            return "Kubernetes DevSecOps";
-        }
+    @GetMapping("/compare/{value}")
+    String compareToFifty(@PathVariable int value) {
+        return value > 50 ? "Greater than 50" : "Smaller than or equal to 50";
+    }
 
-        @GetMapping("/compare/{value}")
-        String compareToFifty(@PathVariable int value) {
-            return value > 50 ? "Greater than 50" : "Smaller than or equal to 50";
-        }
-
-        @GetMapping("/increment/{value}")
-        int increment(@PathVariable int value) {
-            try {
-                ResponseEntity<String> responseEntity = restTemplate.getForEntity(BASE_URL + '/' + value, String.class);
-                String response = responseEntity.getBody();
-                logger.info("Value Received in Request - {}", value);
-                logger.info("Node Service Response - {}", response);
-                return Integer.parseInt(response);
-            } catch (NumberFormatException e) {
-                logger.error("Error parsing response to integer. Value received: {}, Error: {}", value, e.getMessage());
-                throw new ResponseParseException("Failed to parse the response from Node Service for value: " + value, e);
-            }
+    @GetMapping("/increment/{value}")
+    int increment(@PathVariable int value) {
+        try {
+            ResponseEntity<String> responseEntity = restTemplate.getForEntity(BASE_URL + '/' + value, String.class);
+            String response = responseEntity.getBody();
+            logger.info("Value Received in Request - {}", value);
+            logger.info("Node Service Response - {}", response);
+            return Integer.parseInt(response);
+        } catch (NumberFormatException e) {
+            logger.error("Error parsing response to integer. Value received: {}, Error: {}", value, e.getMessage());
+            throw new ResponseParseException("Failed to parse the response from Node Service for value: " + value, e);
         }
     }
 
