380dea75c6857d2e281b48339710811b9abdde27
Abdallah Musa <abdhalla.musa@fefundinfo.com>
2024-05-30 19:22:15 +0100

    updating bash script

--- trivy-docker-image-scan.sh
+++ trivy-docker-image-scan.sh
@@ -1,23 +1,24 @@
 #!/bin/bash
+
 dockerImageName=$(awk 'NR==1 {print $2}' Dockerfile)
 echo $dockerImageName 
 
-docker run --rm -v $WORKSPACE:/root/.cache/aquasec/trivy:0.51.1 -q image-exit-code 0-severity HIGH-Light $dockerImageName
-docker run --rm -v $WORKSPACE:/root/.cache/aquasec/trivy:0.51.1 image exit-code 1-severity CRITICAL --light $dockerImageName
+# Ensure Docker is logged in if the image is private
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
-    #Trivy scan result processing
-    exit_code=$?
+# Run Trivy scan
+docker run --rm -v $WORKSPACE:/root/.cache/aquasec/trivy:0.51.1 aquasec/trivy:0.51.1 image --exit-code 0 --severity HIGH --light $dockerImageName
+docker run --rm -v $WORKSPACE:/root/.cache/aquasec/trivy:0.51.1 aquasec/trivy:0.51.1 image --exit-code 1 --severity CRITICAL --light $dockerImageName
 
-    echo "Exit Code : $exit_code"
+# Trivy scan result processing
+exit_code=$?
 
-    #Check scan results
-    if      
-    [[ "${exit_code}" -eq 1 ]]; 
+echo "Exit Code : $exit_code"
 
-    then
-
+# Check scan results
+if [[ "${exit_code}" -eq 1 ]]; then
     echo "Image scanning failed. Vulnerabilities found"
     exit 1
-    else 
-        echo "Image scanning passed. No CRITICAL vulnerabilities found"
-    fi;
+else 
+    echo "Image scanning passed. No CRITICAL vulnerabilities found"
+fi
