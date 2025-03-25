5286b0688a1d9d789d53c2ece1b28cefad9a868f
suhailzap <suhailsap69@gmail.com>
2025-03-25 21:11:01 +0300

    revert all

--- src/test/java/com/devsecops/NumericApplicationTests.java
+++ src/test/java/com/devsecops/NumericApplicationTests.java
@@ -1,6 +1,7 @@
 package com.devsecops;
 
-import org.junit.jupiter.api.Test;
+
+import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,35 +11,48 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+//import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+//import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
-class NumericApplicationTests { // Removed 'public'
+public class NumericApplicationTests {
 
     @Autowired
     private MockMvc mockMvc;
 
     @Test
-    void smallerThanOrEqualToFiftyMessage() throws Exception { // Removed 'public'
-        this.mockMvc.perform(get("/compare/50"))
-                .andDo(print())
-                .andExpect(status().isOk())
+    public void smallerThanOrEqualToFiftyMessage() throws Exception {
+        this.mockMvc.perform(get("/compare/50")).andDo(print()).andExpect(status().isOk())
                 .andExpect(content().string("Smaller than or equal to 50"));
     }
 
     @Test
-    void greaterThanFiftyMessage() throws Exception { // Removed 'public'
-        this.mockMvc.perform(get("/compare/51"))
-                .andDo(print())
-                .andExpect(status().isOk())
+    public void greaterThanFiftyMessage() throws Exception {
+        this.mockMvc.perform(get("/compare/51")).andDo(print()).andExpect(status().isOk())
                 .andExpect(content().string("Greater than 50"));
     }
     
-    @Test
-    void welcomeMessage() throws Exception { // Removed 'public'
-        this.mockMvc.perform(get("/"))
-                .andDo(print())
-                .andExpect(status().isOk())
+    
+
+@Test
+    public void welcomeMessage() throws Exception {
+        this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk())
                 .andExpect(content().string("Kubernetes DevSecOps"));
     }
+    
+
 }
